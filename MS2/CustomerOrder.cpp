fb3443d9d9a53b0838051eea99cec02a7f7d3550
Ching Wei Lai <103228835+1640Wei@users.noreply.github.com>
2023-09-13 14:25:10 -0500

    Add files via upload

---
+++ MS2/CustomerOrder.cpp
@@ -0,0 +1,139 @@
+/***********************************************************************
+// OOP345 MS2:
+// File	CustomerOrder.cpp
+// Version 1.0
+// Author	Ching Wei Lai
+// Student ID 136893211
+// Email  cwlai6@myseneca.ca
+//
+// Authenticity Declaration:
+// I declare this submission is the result of my own work and has not been
+// shared with any other student or 3rd party content provider. This submitted
+// piece of work is entirely of my own creation.
+/////////////////////////////////////////////////////////////////
+***********************************************************************/
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include "CustomerOrder.h"
+#include "Station.h"
+#include "Utilities.h"
+using namespace std;
+
+namespace sdds{
+	size_t CustomerOrder::m_widthField = 0;
+
+	CustomerOrder::CustomerOrder(const string& str){
+		Utilities temp;
+		bool more = false;
+		size_t pos = 0u;
+		//If the string str is empty ---> set to empty state
+		if (str.empty()){
+			m_name = "";
+			m_product = "";
+			m_cntItem = 0;
+			m_lstItem = nullptr;
+		}
+		else{
+			//Otherwise, the m_name is extracted from the string str
+			m_name = temp.extractToken(str, pos, more);
+			if (more){
+				m_product = temp.extractToken(str, pos, more);
+			}
+			string newItems = str.substr(pos, str.length() - pos);
+			m_cntItem = (count(newItems.begin(), newItems.end(), temp.getDelimiter())) + 1;
+			//DMA for the list of Items
+			m_lstItem = new Item * [m_cntItem]; 
+			for (size_t i = 0u; i < m_cntItem && more; i++)
+				m_lstItem[i] = new Item(temp.extractToken(str, pos, more));  //DMA
+			if (m_widthField < temp.getFieldWidth())
+				m_widthField = temp.getFieldWidth();
+		}
+	}
+
+	CustomerOrder::CustomerOrder(CustomerOrder& src){
+		throw "Error!";
+	}
+
+	CustomerOrder::CustomerOrder(CustomerOrder&& src)noexcept{
+		*this = std::move(src);
+	}
+
+	CustomerOrder& CustomerOrder::operator=(CustomerOrder&& src)noexcept{
+		if (this != &src){
+			for (auto i = 0u; i < m_cntItem; i++){
+				delete m_lstItem[i];
+			}
+			delete[] m_lstItem;
+			//shallow copy
+			m_name = src.m_name;
+			m_product = src.m_product;
+			m_cntItem = src.m_cntItem;
+			m_lstItem = src.m_lstItem;
+			//set to empty state
+			src.m_name = "";
+			src.m_product = "";
+			src.m_cntItem = 0;
+			src.m_lstItem = nullptr;
+		}
+		return *this;
+	}
+
+	CustomerOrder::~CustomerOrder(){
+		for (auto i = 0u; i < m_cntItem; i++){
+			delete m_lstItem[i];
+		}
+		delete[] m_lstItem;
+	}
+
+	bool CustomerOrder::isOrderFilled() const{
+		bool check = true;
+		for (unsigned i = 0; i < m_cntItem; i++){
+			if (!m_lstItem[i]->m_isFilled){
+				check = false;
+			}
+		}
+		return check;
+	}
+
+	bool CustomerOrder::isItemFilled(const std::string& itemName) const{
+		bool check = true;
+		for (unsigned int i = 0; i < m_cntItem; i++){
+			if (m_lstItem[i]->m_itemName == itemName)
+			{
+				return m_lstItem[i]->m_isFilled;
+			}
+		}
+		return check;
+	}
+
+	void CustomerOrder::fillItem(Station& station, std::ostream& os){
+		//check every items in the order
+		for (unsigned int i = 0; i < m_cntItem; i++){
+			if (m_lstItem[i]->m_itemName == station.getItemName()){
+				if (station.getQuantity() > 0){
+					//substract 1 from the inventory
+					station.updateQuantity(); 
+					m_lstItem[i]->m_serialNumber = station.getNextSerialNumber();
+					m_lstItem[i]->m_isFilled = true;
+					os << "    Filled " << m_name << ", " << m_product << " [" << m_lstItem[i]->m_itemName << "]" << std::endl;
+				}
+				else{
+					os << "    Unable to fill " << m_name << ", " << m_product << " [" << m_lstItem[i]->m_itemName << "]" << std::endl;
+				}
+
+			}
+		}
+	}
+	void CustomerOrder::display(std::ostream& os) const{
+		os << m_name << " - " << m_product << std::endl;
+		for (unsigned int i = 0; i < m_cntItem; i++){
+			os << "[" << setw(6) << setfill('0') << m_lstItem[i]->m_serialNumber << "] ";
+		   os << setw(m_widthField) << setfill(' ') << left << m_lstItem[i]->m_itemName << " - ";
+			if (m_lstItem[i]->m_isFilled) os << "FILLED" << endl;
+			else os << "TO BE FILLED" << endl;
+		}
+	}
+}
